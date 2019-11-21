64f83119e4d483afc07f3a680e47fbf5594ef01e
chromatices <pjh403@naver.com>
2019-11-21 23:15:35 +0900

    for문 제거 및 ap2 torch.no_grad() 테스트

--- utils/ap2.py
+++ utils/ap2.py
@@ -1,10 +1,13 @@
 import torch
 
 
-def approximate_power_of_two(x: torch.tensor):
+def approximate_power_of_two(x: torch.tensor) -> torch.tensor:
     with torch.no_grad():
         return torch.sign(x) * 2 ** torch.round(torch.log2(torch.abs(x)))
 
 
 if __name__ == "__main__":
-    print(approximate_power_of_two(torch.randn(3,3)))
+    a = torch.randn(3, 3, requires_grad=True)
+    print(a.requires_grad)
+    print(approximate_power_of_two(a).requires_grad)
+
